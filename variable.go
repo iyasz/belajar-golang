1e46fc0362c6cfbffdb10e93103afe9be58c493b
Yasz Avellia <zakamaragames@gmail.com>
2022-09-18 21:08:13 +0700

    pendefinisian versi 3

--- variable.go
+++ variable.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	//versi 1
 	var namaLengkap string
 	var umur int
 
@@ -15,10 +16,18 @@ func main() {
 	umur = 16
 	fmt.Println(umur)
 
+	//versi 2
 	var namAnime = "Elaina"
 	fmt.Println(namAnime)
 
 	var umurAnime = 17
 	fmt.Println(umurAnime)
+
+	//versi 3
+	namaWf := "Arisu Sakayanagi"
+	fmt.Println(namaWf)
+
+	umurWf := 16
+	fmt.Println(umurWf)
 
 }
