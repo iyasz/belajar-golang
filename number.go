29567a947d40966132b1a1610417b2906ffa0152
Yasz Avellia <zakamaragames@gmail.com>
2022-09-18 17:04:30 +0700

    tipe data number integer and float

--- number.go
+++ number.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(60 + 60)
-	fmt.Println(60 - 60)
-	fmt.Println(60 * 60)
+	fmt.Println("Satu :", 1)
+	fmt.Println("Dua :", 2)
+	fmt.Println("Tiga koma lima :", 3.5)
 }
