ad85c9315dce228a21fe9fca0eaffd18ea6b1c69
Yasz Avellia <zakamaragames@gmail.com>
2022-09-18 20:59:38 +0700

    menambah var number

--- variable.go
+++ variable.go
@@ -4,11 +4,15 @@ import "fmt"
 
 func main() {
 	var namaLengkap string
+	var umur int
 
 	namaLengkap = "Muhammad Ilyasa"
 	fmt.Println(namaLengkap)
 
 	namaLengkap = "Yasz Avellia"
 	fmt.Println(namaLengkap)
+
+	umur = 16
+	fmt.Println(umur)
 
 }
