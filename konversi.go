b9b5f7160310d9db12e21a88d50155c0b69f4eac
Yasz Avellia <zakamaragames@gmail.com>
2022-09-19 11:03:36 +0700

    konversi int

---
+++ konversi.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var anime int64 = 23423
+	var nggih int32 = int32(anime)
+
+	var ytta int16 = int16(nggih)
+
+	fmt.Println(anime)
+	fmt.Println(nggih)
+	fmt.Println(ytta)
+
+}
