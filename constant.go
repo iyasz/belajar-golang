eb12c7fd4e5d4a722de43b6164634b8aef79a4cb
Yasz Avellia <zakamaragames@gmail.com>
2022-09-19 10:51:11 +0700

    kombinasi var dengan const

--- constant.go
+++ constant.go
@@ -5,4 +5,9 @@ import "fmt"
 func main() {
 	const anime string = "yang tau tau aja"
 	fmt.Println(anime)
+
+	const nama = "muhammad ilyasa"
+	var kelas = "XI RPL"
+	fmt.Println(nama)
+	fmt.Println(kelas)
 }
